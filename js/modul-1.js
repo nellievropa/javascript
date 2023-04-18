69834f23b548c57e361425a04d7e3b1fca7bf9fe
nellievropa <nellievropa23@gmail.com>
2023-04-18 21:08:07 +0300

    початок

---
+++ js/modul-1.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const totalPrice = 200.74;
+
+
+
+console.log('price:', totalPrice);
+
+const type = typeof true;
+console.log(type);
+
+// console.log('before');
+// alert('wywywyw');
+// console.log('after');
+
+// const message = 'Хотите продлить подписку?';
+
+// const shouldRenew = confirm(message);
+// console.log(shouldRenew);
+
+// let quantity = prompt('введите количество товара:');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '50px';
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth:', elementWidth);
+
+// let elementHeight = '200.58px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight:', elementHeight);
+
+let salary = 1425.2365478;
+// єто 2 строки
+// salary = salary.toFixed(2);
+// salary = Number(salary);
+
+// можно записать в одну строку
+
+// salary = Number(salary.toFixed(3));
+
+// console.log(salary.toFixed());
+// console.log(salary);
+
+// можно записать еще и так
+// console.log(Number(salary.toFixed(4)));
+
+// console.log(Number('witovn'));
+
+// возведение в степень
+// const base = 2;
+// const power = 5;
+
+// const result = Math.pow(base, power);
+
+// или тоже
+console.log(2 ** 3);
+
+// const result = Math.pow(2, 4);
+
+// console.log(result);
+
+// квадратний корень
+
+// Math.sqrt()
+
+// 1.Попросить ввести число и сохранить в переменную
+// let base = prompt('Дайте число');
+// base = Number(base);
+
+// console.log(base);
+
+// 2.Попросить ввести степень и сохранить в переменную
+// let power = prompt('Дайте степень');
+// power = Number(power);
+
+// console.log(power);
+// 3. возвести в степень и вівести результат
+// const result = base ** power;
+
+// console.log(result);
+
+// const max = 50;
+// const min = 30;
+// const result = Math.round(Math.random() * (max - min) + min);
+
+// console.log(result);
+// округляет
+// Math.round
+
+// можно использовать, когда нужно мнять цвета фона на сайте
+// const colors = ['tomato', 'yellow', 'green', 'skyblue', 'pink'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
